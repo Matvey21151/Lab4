1adfa1d83e6c561ef63c158a8f477acaaf059ba5
Push0CHKA <push-trust@yandex.ru>
2021-11-26 17:59:32 +0300

    gg

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,8 +1,8 @@
 // Copyright 2021 Your Name <your_email>
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef INCLUDE_UTILS_HPP_
+#define INCLUDE_UTILS_HPP_
 
 void start();
 
-#endif // INCLUDE_EXAMPLE_HPP_
+#endif // INCLUDE_UTILS_HPP_
